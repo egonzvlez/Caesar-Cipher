2645fe51c8cb60136ff81149baabe95fb1294c91
Enrique Gonzalez <egonzvlez@Enriques-MacBook-Pro.local>
2024-03-17 21:29:04 -0700

    Progress done. Update main.py
    i

---
+++ main.py
@@ -0,0 +1,8 @@
+alphabet = []
+
+directin = input("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+text = input("Type your message: \n".lower()
+shift = int(input("Type the shift number: n\"))
+
+def encrypt(text, shift):
+
